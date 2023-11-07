801f7e9b3020610d86ca0cf4696ea82cb9fd4334
mr-silent404 <androgeme0@gmail.com>
2023-11-07 14:53:01 +0200

    finishing the setup file

--- setup.sh
+++ setup.sh
@@ -1,3 +1,28 @@
 sudo apt update
 sudo apt upgrade
 sudo apt install subfinder -y
+sudo apt install amass -y
+sudo apt install gobuster -y
+sudo apt install golang -y
+sudo apt install assetfinder -y
+sudo apt install hakrawler -y
+sudo apt install arjun -y
+sudo apt install gospider -y
+sudo apt install dirb -y
+sudo apt install ffuf -y
+
+
+go install github.com/lc/gau/v2/cmd/gau@latest
+sudo mv ~/go/bin/gau /usr/local/bin/
+
+go install github.com/projectdiscovery/notify/cmd/notify@latest
+sudo mv ~/go/bin/notify /usr/local/bin/
+
+go install github.com/projectdiscovery/httpx/cmd/httpx@latest
+sudo mv ~/go/bin/httpx /usr/local/bin/
+
+cd ~
+wget https://github.com/lc/gau/blob/master/.gau.toml
+
+go install github.com/projectdiscovery/katana/cmd/katana@latest
+sudo mv ~/go/bin/katana /usr/local/bin/
